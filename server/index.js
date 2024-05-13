aab838031b77c7a660227125370c817d32a83eca
selveresta <artem.lypnyk@softsculptor.net>
2024-05-13 11:50:42 +0300

    asdfasdf

--- server/index.js
+++ server/index.js
@@ -7,6 +7,17 @@ const port = 5000;
 app.use(json());
 app.use(cors());
 
+app.use(function (req, res, next) {
+	//Enabling CORS
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+	);
+	next();
+});
+
 app.get("/", (req, res) => {
 	const currectCount = fs.readFileSync("./count.txt");
 	console.log(Number(currectCount));
