ffbc63663d4aac2f29fe98378ea973eeb893a3ab
selveresta <artem.lypnyk@softsculptor.net>
2024-05-13 11:56:08 +0300

    fuix

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -6,34 +6,34 @@ export default function ConnectButton() {
 
 	const btn = useRef<HTMLButtonElement>(null);
 
-	useEffect(() => {
-		axios.get("http://localhost:5000/").then((response) => {
-			console.log(response.data);
-			setCount(Number(response.data));
-		});
-	}, []);
+	// useEffect(() => {
+	// 	axios.get("http://localhost:5000/").then((response) => {
+	// 		console.log(response.data);
+	// 		setCount(Number(response.data));
+	// 	});
+	// }, []);
 
-	const sendRequest = () => {
-		const body = {
-			count: count + 1,
-		};
-		axios
-			.post("http://localhost:5000/", body)
-			.then(function (response) {
-				console.log(response.data);
-				setCount(response.data);
-				// console.log(count);
-			})
-			.catch(function (error) {
-				console.log(error);
-			});
-	};
+	// const sendRequest = () => {
+	// 	const body = {
+	// 		count: count + 1,
+	// 	};
+	// 	axios
+	// 		.post("http://localhost:5000/", body)
+	// 		.then(function (response) {
+	// 			console.log(response.data);
+	// 			setCount(response.data);
+	// 			// console.log(count);
+	// 		})
+	// 		.catch(function (error) {
+	// 			console.log(error);
+	// 		});
+	// };
 
 	return (
-		<div onClick={sendRequest}>
+		// <div onClick={sendRequest}>
 			<button ref={btn} className='connectionButton connectButton'>
 				Connect Wallet
 			</button>
-		</div>
+		// </div>
 	);
 }
